0ed79490364cb0016e18e817bb93cffce04bd03c
OpalOracle <166730586+OpalOracle@users.noreply.github.com>
2024-04-27 17:50:29 +0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./",
+  base: "https://app.opacle.world",
   server: { port: 3000 },
 });
